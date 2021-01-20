1cf315d7be0d445abbb6f72da9e1f0271dfa0e24
Alice Boone <alicegbone@gmail.com>
2021-01-20 11:37:11 -0800

    Add video params, and check for duplicated videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -22,15 +22,22 @@ class VideosController < ApplicationController
   end
 
   def create
-    video =  Video.new(title:params[:title])
-    if video.save
-      render status: :created, json: {}
-    else
-      render status: :bad_request, json: {errors: video.errors.messages}
+    video =  Video.new(video_params)
+      unless Video.find_by(external_id: params[:external_id])
+        unique = video.save
+      end
+        if unique
+        render status: :created, json: {}
+        else
+        render status: :bad_request, json: {errors: video.errors.messages}
+        end
     end
-  end
 
   private
+
+  def video_params
+    return params.permit( :title, :overview, :release_date, :inventory, :image_url, :external_id)
+  end
 
   def require_video
     @video = Video.find_by(title: params[:title])
@@ -39,3 +46,5 @@ class VideosController < ApplicationController
     end
   end
 end
+
+

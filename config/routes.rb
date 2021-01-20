08cbd24c87218b146c2a8b1f2450076bf60cf1ef
Alice Boone <alicegbone@gmail.com>
2021-01-19 16:02:47 -0800

    Route to add video

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :videos, only: [:index, :show], param: :title
 
+  post "/videos/:title", to: "videos#add_videos", as: "add_videos"
   post "/rentals/:title/check-out", to: "rentals#check_out", as: "check_out"
   post "/rentals/:title/return", to: "rentals#check_in", as: "check_in"
   get "/rentals/overdue", to: "rentals#overdue", as: "overdue"
